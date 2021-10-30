0074cb8905b366ee65ecac91a03e5ad90e019294
ralexrivero <ralexrivero@gmail.com>
2021-10-30 16:32:08 -0300

    temporal PATH

---
+++ 011-temporary_PATH.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# temporary change of path, only while the script is running, after the path back to previous values
+echo "The PATH is $PATH"
+PATH="/ralex"
+echo "The PATH is $PATH"
+exit 0

fc542db79b7a3f21529e2bb45429163d461f920a
ralexrivero <ralexrivero@gmail.com>
2021-10-30 11:07:16 -0300

    passing parameters with meaning

--- 004-parameters.sh
+++ 004-parameters.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 # Passing parameters
-echo Parameter 0: $0
+# give some meaning to parameters with names
+PROGRAM_NAME=$0
+NAME=$1
+DATE=$(date)
+echo Program name 0: $PROGRAM_NAME
+echo Hello $NAME, today is $DATE

3aa757845610caefd27a85bc01775ba7ca7bee9b
ralexrivero <ralexrivero@gmail.com>
2021-10-31 02:33:00 -0300

    more environment variables

---
+++ 012-more_env_var.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# show more environment variables
+echo "Environment variables"
+echo "HOME: $HOME"
+echo "PATH: $PATH"
+echo "HOSTNAME: $HOSTNAME"
+echo "SHELL: $SHELL"
+echo "EDITOR: $EDITOR"
+echo "USER: $USER"
+echo "TERM: $TERM"
+exit 0
